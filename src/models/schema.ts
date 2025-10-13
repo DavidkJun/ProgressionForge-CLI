98389788e1296db4bfabbe7279e40346d3c782ce
Davyd Krykota <krikotadavid957@gmail.com>
2025-10-13 23:30:47 +0300

    feat: add validation using zod

---
+++ src/models/schema.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+export const progressionParamsSchema = z.object({
+  incrementCoefficient: z.number(),
+});
+
+export const exerciseSchema = z.object({
+  name: z.string(),
+  type: z.enum(['bodyweight', 'weight']),
+  reps: z.number(),
+  sets: z.number(),
+  startCoefficient: z.number(),
+  progressionModel: z.enum(['linear', 'percentage', 'wave', 'block']),
+  progressionParams: progressionParamsSchema,
+});
+
+export const recipePlanSchema = z.object({
+  planName: z.string(),
+  durationWeeks: z.number(),
+  bodyWeightKg: z.number(),
+  exercises: z.array(exerciseSchema),
+});
