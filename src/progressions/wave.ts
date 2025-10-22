97b46acbde5eac31e53833fe494d6b58bca03074
Davyd Krykota <krikotadavid957@gmail.com>
2025-10-22 15:12:00 +0300

    feat: implement wave progression

---
+++ src/progressions/wave.ts
@@ -0,0 +1,34 @@
+import { roundToNearest } from '../utils/math.js';
+
+export interface waveProgressionParams {
+  weeksUp: number;
+  upCoefficient: number;
+  downCoefficient: number;
+}
+
+export interface WaveParams {
+  initialWeight: number;
+  durationWeeks: number;
+  progressionParams: waveProgressionParams;
+}
+
+export const waveProgression = (params: WaveParams): number[] => {
+  const { initialWeight, durationWeeks, progressionParams } = params;
+  const { weeksUp, upCoefficient, downCoefficient } = progressionParams;
+  const weeklyWeights = [];
+  let currentWeight = initialWeight;
+  let upWeekCounter = 0;
+  const upStepKg = initialWeight * upCoefficient;
+  const downStepKg = initialWeight * downCoefficient;
+  for (let i = 0; i < durationWeeks; i++) {
+    weeklyWeights.push(roundToNearest(currentWeight, 2.5));
+    if (upWeekCounter < weeksUp) {
+      currentWeight += upStepKg;
+      upWeekCounter++;
+    } else {
+      upWeekCounter = 0;
+      currentWeight -= downStepKg;
+    }
+  }
+  return weeklyWeights;
+};
